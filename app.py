fa4928c6aadd71323505af31c36d6db53e3db66b
Ubuntu <ubuntu@ip-172-31-80-78.ec2.internal>
2019-06-30 20:01:18 +0000

    initial commit

--- app.py
+++ app.py
@@ -8,3 +8,8 @@ app=Flask(__name__)
 
 def index():
     return "<h1>Hello world.</h1><h2>This is a recipe book.</h2>"
+    
+    
+if __name__ =="__main__":
+    app.run(host=os.environ.get("IP"), port=int(os.environ.get("PORT")), debug=True)    
+    
