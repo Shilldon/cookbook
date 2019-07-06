b25e94065d43bdafb9cd01cb13129555f0a811d8
Ubuntu <ubuntu@ip-172-31-80-78.ec2.internal>
2019-07-06 23:22:52 +0000

    implemented data request from form and iterate through data to put into correct format for DB

--- app.py
+++ app.py
@@ -20,11 +20,49 @@ def index():
 def add_recipe():
     return render_template("add_recipe.html", title_text='Add recipe')
 
-@app.route("/insert_recipe")
+@app.route("/insertrecipe", methods=['POST'])
+def insertrecipe():
+    recipes=mongo.db.recipeDB
+    #request form data not in flat format - arrays are created in the form for ingredients and allergen information
+    new_recipe=request.form.to_dict(flat=False)
+    ingredients=[]
+    ingredient={}
+    for key, key_name in new_recipe.items():
+        #flatten dict for non-array items which have become arrays through request object
+        if key!='type' and key!='amount' and key!='unit' and key!='allergens':
+            new_recipe[key]=new_recipe[key][0] 
+        elif key=='type':
+            #cycle through the ingredient information and create objects for each ingredient with key values of ingredient type, amount and units
+            i=0
+            for ingredient_type in key_name:
+                ingredient={ 'type' : ingredient_type , 'amount' : new_recipe['amount'][i] , 'unit' : new_recipe['unit'][i] }
+                print(ingredient)
+                #insert ingredient object into array of ingredients
+                ingredients.append(ingredient)
+                print(ingredients)
+                i+=1       
+    #insert the new ingredients array into the new_receipe dict
+    new_recipe['ingredients']=ingredients            
+    
+    #need to remove the type, amount and unit arrays from the dict having imported them into ingredients objects
+    #done by error testing because ingredients may not have been added via the form
+    try:
+        del new_recipe['type']
+    except KeyError:
+        pass
+    try:
+        del new_recipe['amount']
+    except KeyError:
+        pass    
+    try:
+        del new_recipe['unit']
+    except KeyError:
+        pass    
+    
+    print("ingredients=", ingredients)        
+    print("amended recipe=",new_recipe)
 
-def insert_recipe():
-    recipes=mongo.db.recipes
-    new_recipe=request.form.to_dict
+
     recipes.insert_one(new_recipe)
     return redirect(url_for('index'))
 
