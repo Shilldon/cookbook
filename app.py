97e9cdd14b76c6d767176bfb8aba658dd55dbb3f
Ubuntu <ubuntu@ip-172-31-80-78.ec2.internal>
2019-07-19 21:52:27 +0000

    added code to create relational database for countries and authors to aid sorting by category

--- app.py
+++ app.py
@@ -17,6 +17,7 @@ mongo=PyMongo(app)
 def index():
     print("blanking session variable")
     session["filters"]={}
+    session["sort"]=""
     return render_template("index.html",title_text='Perfect dishes on demand')
 
 @app.route("/add_recipe")
@@ -29,11 +30,16 @@ def insertrecipe():
     #check whether the user is adding a new recipe or editting a recipe
     edit=request.args.get('edit',None)
     
+
+        
+    recipes=mongo.db.recipeDB
+    
     #if editing a recipe get the ID of the recipe to edit
     if edit=='True':
-        recipe_id=request.args.get('recipe_id',None)
+        recipe_id=ObjectId(request.args.get('recipe_id'))
+    else:
+        recipe_id=recipes.insert({'name':'new'})
     
-    recipes=mongo.db.recipeDB
     #request form data not in flat format - arrays are created in the form for ingredients and allergen information
     new_recipe=request.form.to_dict(flat=False)
     ingredients=[]
@@ -44,7 +50,6 @@ def insertrecipe():
         #flatten dict for non-array items which have become arrays through request object
         if key!='type' and key!='amount' and key!='unit' and key!='allergens':
             new_recipe[key]=new_recipe[key][0]
-
             if key=='favourite':
                 if new_recipe[key].lower() =='true':
                     new_recipe[key]=True
@@ -61,7 +66,6 @@ def insertrecipe():
                 cook_time=new_recipe[key]*60
             if key=='minutes':
                 cook_time=cook_time+new_recipe[key]
-            
         elif key=='type':
             #cycle through the ingredient information and create objects for each ingredient with key values of ingredient type, amount and units
             i=0
@@ -76,6 +80,16 @@ def insertrecipe():
     #insert the new ingredients array into the new_receipe dict
     new_recipe['ingredients']=ingredients            
     
+    categories=["country","author"]
+    for category in categories:
+        if category=="country":
+            database=mongo.db.countriesDB
+        elif category=="author":
+            database=mongo.db.authorDB
+        update_country_database(edit,recipe_id,recipes,new_recipe,category,database)      
+
+    print("come bvack")        
+
     #need to remove the type, amount and unit arrays from the dict having imported them into ingredients objects
     #done by error testing because ingredients may not have been added via the form
     try:
@@ -94,12 +108,56 @@ def insertrecipe():
     if edit=='True':
         recipes.update({'_id':ObjectId(recipe_id)},new_recipe)
     else:
-        recipes.insert_one(new_recipe)
-        print("New recipe added",new_recipe)
-    return render_template('display_recipe.html',title_text='Your recipe',recipe=new_recipe,added="true")
+        recipes.update({'_id':ObjectId(recipe_id)},new_recipe)
+        
+    return redirect(url_for('show_recipe',recipe_id=recipe_id,added="true"))
+
+def update_country_database(edit,recipe_id,recipes,new_recipe,category,country_db):
+    print("edit ",edit)
+    print("recipe_id ",recipe_id)
+    print("reciopes ",recipes)
+    print("new recipe ",new_recipe)
+    print("category ", category)
+    #Update country database
+    if edit=="True":
+        current_recipe_doc=recipes.find_one({"_id" :recipe_id})
+    else:
+        current_recipe_doc={}
+
+    if new_recipe[category]:
+        new_country_doc=country_db.find_one({"name" : new_recipe[category]})
+        if new_country_doc!=None:
+            new_country_id=new_country_doc["_id"]
+        else:
+            new_country_id=country_db.insert({"name" : new_recipe[category]})
+            new_country_doc=country_db.find_one({"_id" : new_country_id})
+    else:
+        new_country_doc={}
+        new_country_id=""
+
+    if current_recipe_doc!={}:
+        current_country_id=current_recipe_doc[category]
+        current_country_doc=country_db.find_one({"_id" : current_country_id})
+    else:
+        current_country_id=""
+        current_country_doc={}
+        print("current country_doc=",current_country_doc)
+        
+
+    if new_country_doc!=current_country_doc:
+        if current_country_doc!={}:
+            country_db.update({"_id": current_country_id},{"$pull":{"recipe" : recipe_id}})
+            current_country_doc=country_db.find_one({"_id": current_country_id})
+            if len(current_country_doc["recipe"])==0:
+                country_db.remove({"_id": current_country_id})    
+    
+    if new_country_doc!={}:
+        new_recipe[category]=new_country_doc["_id"]
+        country_db.update({"_id": new_country_id},{"$push":{"recipe" : recipe_id}})
+        
+    return  
 
 @app.route("/search")
-
 def search():
     return render_template("search.html",title_text="Find your favourites")
 
@@ -206,106 +264,54 @@ def recipe_list():
             _recipe_list=mongo.db.recipeDB.find()
 
     #create a list of countries and authors referred to in recipe db to generate filters in recipe_list
-    full_list_of_country_recipes=mongo.db.recipeDB.find({"country":{'$exists':True}})
-    full_list_of_author_recipes=mongo.db.recipeDB.find({ "author":{'$exists':True}})
-    
-    unsorted_country_list=[]
-    unsorted_author_list=[]
-    
-    for recipe in full_list_of_country_recipes:
-        #if the country key value is not blank add it to the list
-        if recipe["country"]:
-            unsorted_country_list.append(recipe["country"]) 
-            
-    for recipe in full_list_of_author_recipes:
-        #if the author key value is not blank add it to the list
-        if recipe["author"]:
-            unsorted_author_list.append(recipe["author"]) 
+    full_list_of_country_recipes=mongo.db.countriesDB.find()
+    full_list_of_author_recipes=mongo.db.authorDB.find()
     
     #remove duplicates from the lists
-    _country_list=list(dict.fromkeys(unsorted_country_list))
-    _author_list=list(dict.fromkeys(unsorted_author_list))
-    
+    _author_list=[]
+    for key in full_list_of_author_recipes:
+        if key=="name":
+            _author_list.append(key["name"])
+    _country_list=[]
+    for key in full_list_of_country_recipes:
+        if key=="name":
+            _country_list.append(key["name"])    
+        
     _total_results=_recipe_list.count()
 
     return render_template("recipe_list.html",title_text=title_text_value,recipes=_recipe_list,countries=_country_list,authors=_author_list,action=_todo,filters=_filter_dict, allergens=allergen_list, sort=_sort, total_results=_total_results)
-'''
-    _sort=request.form.get('sort_by')
-    print("sort=",_sort)
-    
-    #get details of any filters applied
-    if _sort==None:
-        _filter_dict=request.form.to_dict(flat=False)
-        print("got filter dict from form")
-    else:
-        _filter_dict=session["filters"]
-        print("got filter dict from session")
-    
-    allergen_list=[]
 
-    #check if the results are to be filtered using variable passed from recipe_list
-    filter=request.args.get('filter',None)
-    
-    #if so cycle through the dictionary and convert form results to mongo search strings to enable the mongo request to run correctly.
 
-    if filter=='true':
-        if _filter_dict:
-            #if the filters have been submitted from the form, apply them to a session variable to retrieve later
-            session["filters"]=_filter_dict 
-            #flatten dictionary as all results but allergens should not be lists:
-            for key, value in _filter_dict.items():
-                if key !="allergens":
-                    _filter_dict[key]=_filter_dict[key][0]
-        else:
-            #if filters not retrieved from the form, apply from session variable.
-            #This ensures if the user presses 'back' from display recipe the filters on the search list are retained
-            _filter_dict=session["filters"]
-            print("_filter_dict updated from session ",_filter_dict)
-        
-        #check if the results list is to be filtered in order to return appropriate list in mongo request
-        _sort=_filter_dict.get("sort_by","none")
-        
-        if _sort!="none":
-            #need to remove sort_by from the dictionary before applying filters - sort_by is not a valid filter
-            _sort=str(_filter_dict["sort_by"])
-            del _filter_dict["sort_by"]
-            session["sort"]=_sort
-      #  else:
-      #      _sort=session["sort"]
-        
-        #iterate through the filter dictionary and standardise the values for mongoDB search    
-        for key, value in _filter_dict.items():
-            if value=='false':
-                _filter_dict[key]=False
-            elif value=='true':
-                _filter_dict[key]=True
-            elif key=="calories":
-                if int(value)==999:
-                    _filter_dict[key]={'$gt':int(value)}
-                else:
-                    _filter_dict[key]={'$lt':int(value)}    
-            elif key=="cook_time":
-                if value==179:
-                    _filter_dict[key]={'$gt':int(value)}
-                else:
-                    _filter_dict[key]={'$lt':int(value)}  
-            elif key=="allergens":
-                allergen_list=[]
-                loop=0
-                for allergen in _filter_dict[key]:
-                    allergen_list.append(allergen)
-                _filter_dict[key]={  '$nin' : allergen_list }   
-
-'''
 @app.route('/show_recipe/<recipe_id>')
 def show_recipe(recipe_id):
     _recipe=mongo.db.recipeDB.find_one({"_id":ObjectId(recipe_id)})
+    categories=["country","author"]
+    for category in categories:
+        if category=="country":
+            database=mongo.db.countriesDB
+        elif category=="author":
+            database=mongo.db.authorDB
+        if _recipe[category]:
+            _categorydoc=database.find_one({"_id":ObjectId(_recipe[category])})
+            print("category doc",_categorydoc)
+            _recipe[category]=_categorydoc["name"]
+    title_text='Your recipe'
     print("checking session variable=",session["filters"])    
-    return render_template('display_recipe.html',title_text="Your recipe",recipe=_recipe)
+    return render_template('display_recipe.html',title_text='Your recipe',recipe=_recipe,added="true")
 
 @app.route('/edit_recipe/<recipe_id>')
 def edit_recipe(recipe_id):
     _recipe=mongo.db.recipeDB.find_one({"_id":ObjectId(recipe_id)})
+    categories=["country","author"]
+    for category in categories:
+        if category=="country":
+            database=mongo.db.countriesDB
+        elif category=="author":
+            database=mongo.db.authorDB
+        if _recipe[category]:
+            _categorydoc=database.find_one({"_id":ObjectId(_recipe[category])})
+            print("category doc",_categorydoc)
+            _recipe[category]=_categorydoc["name"]    
     return render_template('edit_recipe.html',title_text="Edit your recipes",recipe=_recipe)
 
 @app.route('/delete_recipe',methods=['POST'])
