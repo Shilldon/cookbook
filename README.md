c4976b471534344de11c58ce6e81d31cadda4b47
Ubuntu <ubuntu@ip-172-31-80-78.ec2.internal>
2019-08-05 23:04:52 +0000

    added defensive code - error modal generated if trying to search against empty category. Added background image fade on selet category

--- README.md
+++ README.md
@@ -5,7 +5,9 @@ created Procfile
 
 
 recipe:
+https://www.bbcgoodfood.com/recipes/1514/beef-burgers-learn-to-make
 https://www.bbcgoodfood.com/recipes/5544/the-ultimate-makeover-fish-and-chips
+https://www.bbcgoodfood.com/recipes/1359634/sausage-and-mash
 
 background:
 Photo by Brooke Lark on Unsplash
@@ -16,3 +18,10 @@ defensive - list of authors and countries tested before displaying filter
 
 
 materialize pagination plugin in https://mirjamsk.github.io/materialize-pagination/
+search buttons:
+meal: https://c.pxhere.com/photos/5b/df/blue_plate_breakfast_cuisine_dairy_product_delicious_dining_dining_room_dinner-1516443.jpg!d
+ingredients: https://pxhere.com/en/photo/1433267
+author: https://c.pxhere.com/photos/f4/26/alphabets_ancient_author_data_document_editorial_information_keyboard-1401823.jpg!d
+allergies: https://c.pxhere.com/images/30/d8/1114ac791573938512d436cfd697-1435829.jpg!d
+
+defensive - category search - prevent entering empty search
--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,33 @@
 function myFunc() {
     return ingredient_list
 }
+
+    $(document).ready(function() {
+      $('.sidenav').sidenav();
+      $('select').formSelect();
+      $('.modal').modal({
+        //defensive - prevent modals from being closed by clicking on background - so an option has to be selected
+        dismissible: false,
+      });
+      $('.collapsible').collapsible();
+      $('.dropdown-trigger').dropdown();
+    });
+
+    $("#favourite").on("click", function() {
+      //ensure the favourite variable is a boolean value
+      var favourite = $("#favourite_input").val();
+      if (typeof(favourite) != Boolean) {
+        favourite = (favourite.toLowerCase() == "true");
+      }
+      if (favourite == false) {
+        $("i", this).text("star");
+        $("i", this).addClass("gold-star");
+        $("#favourite_input").val(true);
+      }
+      else {
+        $("i", this).text("star_border");
+        $("i", this).removeClass("gold-star");
+        $("#favourite_input").val(false);
+        favourite = $("#favourite_input").val();
+      }
+    })
